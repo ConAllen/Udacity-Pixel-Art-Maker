094434ca3e319dadb614e591906eebb02c05a462
Your Name <you@example.com>
2018-01-16 17:12:56 +0000

    first commit

---
+++ designs.js
@@ -0,0 +1,39 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+
+$(function(){
+
+
+    // select color
+
+    let colorPicker = $('#colorPicker');
+
+    // select size input
+
+    let input_height = $('#input_height');
+    let input_width = $('input_width');
+
+
+
+
+    function makeGrid() {
+
+        // Your code goes here!
+
+        }
+
+
+}
+
+
+
+
+
+
+
+
+
+
